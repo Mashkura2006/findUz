73d5a6bc2aadec84db95e2988ca9f8f25a37697f
Mashkura2006 <mashkuramirzayoqubova@gmail.com>
2025-07-15 13:31:32 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Home, Showcase } from './components/home.jsx';
+
+function App() {
+
+  return (
+   <>
+   <Home/>
+   <Showcase/>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/home.jsx
@@ -0,0 +1,44 @@
+import { Menu, Search, Volume2 } from "lucide-react"
+import { Link } from "react-router-dom"
+
+
+export const Home = () => {
+return(
+    <nav className="navbar flex justify-between items-center md:px-10 md:py-8 px-8 py-6 w-full h-[100px] shadow-md top-0 left-0 right-0 fixed bg-white">
+        <div className="logo">
+            <h1 className="text-blue-900 font-bold xl:text-[40px] text-[28px]">FIND.UZ</h1>
+        </div>
+        <div className="links menu">
+                <input type="checkbox" className="hidden" id="menu"/>
+            <ul className="flex items-center gap-4 xl:text-xl text-lg font-semibold text-blue-900 ul lg:bg-white bg-blue-900">
+                <button className="btn1 bg-white border-2 text-blue-900 text-xl md:px-8 px-6 rounded-md transition duration-700 md:py-2 py-[5px]">Kirish</button>
+                <li><a href="#">Yo'qolgan</a></li>
+                <li><a href="#">Topib olingan</a></li>
+                <li><a href="#">Biz haqimizda</a></li>
+                <li><a href="#">Aloqa</a></li>
+                <li><a href="#">Registratsiya</a></li>
+            </ul>
+                <label htmlFor="menu" className="label2"></label>
+        </div>
+        <div className="btn flex items-center gap-4">
+            <button className="bg-blue-900 border-2 border-blue-900 text-xl md:px-8 px-6 rounded-md transition duration-700 md:py-2 py-[5px] hover:bg-white hover:text-blue-950 text-white">Kirish</button>
+            <label htmlFor="menu" className="label1"><Menu size={46} color="darkblue"/></label>
+        </div>
+    </nav>
+)
+}
+
+
+export const Showcase = () => {
+    return(
+        <section className="showcase flex items-center justify-center text-center w-full h-screen px-12 bg-blue-50 flex-col leading-relaxed">
+            <h1 className="text-5xl font-semibold text-black leading-snug xl:w-7/12 title">Yo'qolgan buyumlarni topishda eng yaxshi yordamchi!</h1>
+            <div className="bg-blue-50 flex items-center justify-center gap-4 lg:flex-nowrap flex-wrap">
+                <button className="bg-blue-600 rounded-md py-2 px-2"><Link to="#" className="flex text-justify text-xl items-center justify-center"><Search color="white" size={45}/><h2>Yo'qolgan buyumni qidirish</h2></Link></button>
+                <button className="bg-red-600 rounded-md py-2 px-2"><Link to="#" className="flex items-center justify-center gap-2 text-xl"><Volume2 color="white" size={45}/><h2>Topib olingan buyum haqida e'lon berish</h2></Link></button>
+            </div>
+        </section>
+    )
+}
+
+
