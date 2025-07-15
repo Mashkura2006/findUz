8c1b43c255ac1a61b709aa5cb5ab481ab3f37619
Mashkura2006 <mashkuramirzayoqubova@gmail.com>
2025-07-15 16:45:32 +0500

    home page completed

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -30,11 +30,11 @@ return(
 
 export const Showcase = () => {
     return(
-        <section className="showcase flex items-center justify-center text-center w-full h-screen px-12 bg-blue-50 flex-col leading-relaxed">
-            <h1 className="text-5xl font-semibold text-black leading-snug xl:w-7/12 title">Yo'qolgan buyumlarni topishda eng yaxshi yordamchi!</h1>
-            <div className="bg-blue-50 flex items-center justify-center gap-4 lg:flex-nowrap flex-wrap">
-                <button className="bg-blue-600 rounded-md py-2 px-2"><Link to="#" className="flex text-justify text-xl items-center justify-center"><Search color="white" size={45}/><h2>Yo'qolgan buyumni qidirish</h2></Link></button>
-                <button className="bg-red-600 rounded-md py-2 px-2"><Link to="#" className="flex items-center justify-center gap-2 text-xl"><Volume2 color="white" size={45}/><h2>Topib olingan buyum haqida e'lon berish</h2></Link></button>
+        <section className="showcase flex items-center justify-center text-center w-full h-screen sm:px-12 bg-blue-50 flex-col leading-relaxed">
+            <h1 className="xl:text-5xl md:text-4xl text-3xl font-semibold text-black leading-snug xl:w-7/12 title">Yo'qolgan buyumlarni topishda eng yaxshi yordamchi!</h1>
+            <div className="bg-blue-50 flex items-center justify-center gap-4 md:flex-nowrap flex-wrap">
+                <button className="bg-blue-600 rounded-md py-2 px-2 text-white"><Link to="#" className="flex text-justify lg:text-xl text-md items-center justify-center gap-2"><Search color="white" size={35} /><h2 className="text-center">Yo'qolgan buyumni qidirish</h2></Link></button>
+                <button className="bg-red-600 rounded-md py-2 px-2 text-white"><Link to="#" className="flex items-center justify-center gap-2 lg:text-xl text-md"><Volume2 color="white" size={35}/><h2 className="text-center">Topilgan buyumni e'lon qilish</h2></Link></button>
             </div>
         </section>
     )
